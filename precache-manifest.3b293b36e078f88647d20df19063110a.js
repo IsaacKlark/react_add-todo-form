4d1e902884c96fd934d8cae630e8f2ac7daf121c
IsaacKlark <isaackeklarke@gmail.com>
2019-12-10 22:12:51 +0200

    Updates

---
+++ precache-manifest.3b293b36e078f88647d20df19063110a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6345b574de46df1088972038caddfd13",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "6713c9a67d0a3303667d",
+    "url": "/react_add-todo-form/static/css/main.2ead8e9d.chunk.css"
+  },
+  {
+    "revision": "aee9345e8eba4db13a9d",
+    "url": "/react_add-todo-form/static/js/2.11b38f89.chunk.js"
+  },
+  {
+    "revision": "6713c9a67d0a3303667d",
+    "url": "/react_add-todo-form/static/js/main.5e3dc58d.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
